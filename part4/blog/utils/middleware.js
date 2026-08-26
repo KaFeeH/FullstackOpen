94ae3e2a37dffd996f80f621c40b0963ce4c4ad2
KaFeH <fernandokarlos7@gmail.com>
2026-08-25 22:40:39 -0500

    fix: handle blog request errors

--- part4/blog/utils/middleware.js
+++ part4/blog/utils/middleware.js
@@ -1,9 +1,19 @@
-const errorHandler = (error, _request, response, _next) => {
-  if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message})
+const errorHandler = (error, _request, response, next) => {
+  if (error.name === "CastError") {
+    return response.status(400).json({
+      error: "malformatted id",
+    });
   }
-}
+
+  if (error.name === "ValidationError") {
+    return response.status(400).json({
+      error: error.message,
+    });
+  }
+
+  next(error);
+};
 
 module.exports = {
-  errorHandler
-}
+  errorHandler,
+};
