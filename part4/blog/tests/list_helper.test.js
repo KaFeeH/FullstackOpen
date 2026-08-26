b9e8e6fec3db6ade1601bb1ea4ebb40ab8dfe12d
KaFeH <fernandokarlos7@gmail.com>
2026-08-25 22:40:11 -0500

    feat: add solutions 4.3 - 4.7 ✅

---
+++ part4/blog/tests/list_helper.test.js
@@ -0,0 +1,64 @@
+const {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+} = require("../utils/list_helper.js");
+const { listWithOneBlog, listWithManyBlogs } = require("../mocks/blogs.mock.js");
+const { test, describe } = require("node:test");
+const assert = require("node:assert");
+
+test("dummy returns one", () => {
+  const blogs = [];
+  assert.strictEqual(dummy(blogs), 1);
+});
+
+describe("total likes", () => {
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = totalLikes(listWithOneBlog);
+    assert.strictEqual(result, 5);
+  });
+
+  test("when list has multiple blogs, equals the total likes", () => {
+    const result = totalLikes(listWithManyBlogs);
+    assert.strictEqual(result, 97);
+  });
+});
+
+describe("favorite blog", () => {
+  test("when list has many blogs, returns the blog with the most likes", () => {
+    const result = favoriteBlog(listWithManyBlogs);
+    assert.deepStrictEqual(result, {
+      title: "Clean Code",
+      author: "Robert C. Martin",
+      likes: 20,
+    });
+  });
+});
+
+describe("most blogs", () => {
+  test("returns the author with the most blogs", () => {
+    const result = mostBlogs(listWithManyBlogs);
+    assert.deepStrictEqual(result, {
+      author: "Eric S. Raymond",
+      blogs: 2,
+    });
+  });
+});
+
+describe("most likes", () => {
+  test("returns the author whose blogs have the most likes in total", () => {
+    const blogs = [
+      { author: "Author A", likes: 11 },
+      { author: "Author A", likes: 10 },
+      { author: "Author B", likes: 20 },
+    ];
+
+    const result = mostLikes(blogs);
+    assert.deepStrictEqual(result, {
+      author: "Author A",
+      likes: 21,
+    });
+  });
+});
---
+++ part4/blog/utils/list_helper.js
@@ -0,0 +1,80 @@
+const dummy = (_blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  const total = blogs.reduce((prev, curr) => prev + curr.likes, 0);
+  return total;
+};
+
+const favoriteBlog = (blogs) => {
+  const favorite = blogs.reduce((currFav, blog) => {
+    if (!currFav || blog.likes > currFav.likes) {
+      return blog;
+    }
+
+    return currFav;
+  }, null);
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
+};
+
+const mostBlogs = (blogs) => {
+  const blogsByAuthor = new Map();
+
+  for (const { author } of blogs) {
+    const count = (blogsByAuthor.get(author) ?? 0) + 1;
+    blogsByAuthor.set(author, count);
+  }
+
+  let mostAuthor;
+  let mostCount = 0;
+
+  for (const [author, count] of blogsByAuthor) {
+    if (count > mostCount) {
+      mostAuthor = author;
+      mostCount = count;
+    }
+  }
+
+  return {
+    author: mostAuthor,
+    blogs: mostCount,
+  };
+};
+
+const mostLikes = (blogs) => {
+  const likesByAuthor = new Map();
+
+  for (const { author, likes } of blogs) {
+    const count = (likesByAuthor.get(author) ?? 0) + likes;
+    likesByAuthor.set(author, count);
+  }
+
+  let mostAuthor;
+  let mostCount = 0;
+
+  for (const [author, count] of likesByAuthor) {
+    if (count > mostCount) {
+      mostAuthor = author;
+      mostCount = count;
+    }
+  }
+
+  return {
+    author: mostAuthor,
+    likes: mostCount,
+  };
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+};
